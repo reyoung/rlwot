ae5ca6a9946c7b9cd6f3272607e45654d0692884
Yang Yu <josephyu@tencent.com>
2025-12-15 16:39:06 +0800

    complete eval

---
+++ scripts/start_vllm_serve.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pids=()
+
+for i in {0..3}
+do
+    vllm serve Qwen/Qwen3-4B-Instruct-2507 \
+        --enable-lora --served-model-name base \
+        --max-lora-rank 8 --max-loras 10 \
+        --uvicorn-log-level error \
+        --port 5810$i --max-model-len 8192 &
+    pids+=($!)
+done
+
+read -p "Press [Enter] key to stop the servers"
+
+for pid in ${pids[*]}
+do
+    kill $pid
+done
+
+wait
