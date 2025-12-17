e1b1fb5a698f4063d910f9f922c3eb5767a0d1f9
Yang Yu <josephyu@tencent.com>
2025-12-17 14:29:28 +0800

    init commit

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import argparse
+from datetime import datetime
+import gc
 import os
 import random
+import shutil
 import signal
 import sys
+import typing
 import numpy as np
 import torch
 import ray
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import datasets
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,18 +22,49 @@ def parse_args():
     parser.add_argument("--num_engines", type=int, default=4)
     parser.add_argument("--cuda_devices", type=str, default='0,1,2,3')
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--experiment_dir", type=str, default='./experiments')
 
     args = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_devices
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
+    args.experiment_dir = os.path.join(args.experiment_dir, f"{args.model_name.replace('/', '---')}"
+                                       "dapo_math", datetime.now().strftime('%Y%m%d_%H%M%S'))
 
     return args
 
+def save_base_model(args: argparse.Namespace, model_save_dir: str):
+    base_model = AutoModelForCausalLM.from_pretrained(
+        args.model_name,
+        torch_dtype=torch.float16,
+        device_map="cpu",
+        trust_remote_code=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
+    base_model_dir = os.path.join(model_save_dir, "base_model")
+    if os.path.exists(base_model_dir):
+        shutil.rmtree(base_model_dir)
+    os.makedirs(base_model_dir, exist_ok=True)
+    tokenizer.save_pretrained(base_model_dir)
+    base_model.save_pretrained(base_model_dir)
+    del base_model
+    gc.collect()
+
+def _map_dapo_math(example):
+    return {
+        "prompt": example["prompt"],
+        "ground_truth": example["reward_model"]["ground_truth"],
+        "index": example["extra_info"]["index"]
+    }
+
+def load_dataset():
+    train_dataset: datasets.Dataset = datasets.load_dataset("BytedTsinghua-SIA/DAPO-Math-17k")["train"]
+    train_dataset = train_dataset.map(_map_dapo_math)
+    return train_dataset
+
 def main():
     args = parse_args()
-
     ray.init(address="local", include_dashboard=False, ignore_reinit_error=True)
     engines = []
     pgs = []
@@ -52,6 +88,13 @@ def main():
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
+
+    model_save_dir = os.path.join(args.experiment_dir, "models")
+
+    save_base_model(args, model_save_dir)
+
+
+
 
 if __name__ == "__main__":
     main()
